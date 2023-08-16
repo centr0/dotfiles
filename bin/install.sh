10a54630fcba5f06b8c9558e7e58a286097a5cab
Chris Paraiso <cparaiso@unicon.net>
2023-08-16 15:42:27 -0700

    added nvim config init

---
+++ bin/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "==============================================="
+echo "Running install..."
+zshrc_path="$HOME/.zshrc"
+zshrc_add='source $HOME/src/dotfiles/zshrc'
+if ! grep -qF "$zshrc_add" $zshrc_path; then
+    # Append the code if the search line is not found
+    echo "# dotfiles" >> "$zshrc_path"
+    echo "$zshrc_add" >> "$zshrc_path"
+    echo "Dotfiles have been installed."
+else
+    echo "Dotfiles have already been installed. No changes made."
+fi
+
+echo "Installing nvim configuration..."
+ln -s "$HOME/src/dotfiles/nvim" "$HOME/.config/nvim"
+echo "Configuration for nvim has been installed."
+
+echo "==============================================="
