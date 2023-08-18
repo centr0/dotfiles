59bf680311373356ef2a9ad6bffeb788011d975e
Chris Paraiso <cparaiso@unicon.net>
2023-08-17 22:21:42 -0700

    add script to pull updates and push to repo

---
+++ bin/pull.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# pulls any updates from nvchad
+echo "========================================="
+
+timestamp=$(date "+%Y%m%d%H%M%S")
+nvm_custom="$HOME/.config/nvim/lua/custom"
+nvm_dest="$HOME/src/dotfiles/nvchad/"
+
+cp -rvf "$nvm_custom"/* "$nvm_dest"
+
+echo "Pulled your custom updates from ~/.config/nvim..."
+echo "Checking for changes..."
+
+cd "$HOME/src/dotfiles/nvchad/"
+
+git_status=$(git status)
+
+if [[ "$git_status" == *"nothing to commit, working tree clean"* ]]; then
+    echo "No changes found in the repository. Exiting..."
+else
+    git add .
+    git commit -m "nvim custom settings update ${timestamp}"
+    git push origin master
+    echo "Update complete."
+fi
+
+echo "========================================="
+
