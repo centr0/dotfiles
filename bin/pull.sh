df4c584127158d2c6b2666fbfc995a010ccd40fe
Chris Paraiso <cparaiso@unicon.net>
2023-08-19 02:29:08 -0700

    dotfiles update 20230819022908

--- bin/pull.sh
+++ bin/pull.sh
@@ -7,24 +7,24 @@ nvm_custom="$HOME/.config/nvim/lua/custom"
 nvm_dest="$HOME/src/dotfiles/nvchad/"
 
 cp -rf "$nvm_custom"/* "$nvm_dest"
-echo "Pulled your custom updates from ~/.config/nvim..."
+echo "-- Pulled your custom updates from ~/.config/nvim..."
 
 cp "$HOME/.tmux.conf" "$HOME/src/dotfiles/tmux/tmux.conf"
-echo "Pulled your tmux configuration..."
+echo "-- Pulled your tmux configuration..."
 
-echo "Checking for changes..."
+echo "-- Checking for changes..."
 
 cd "$HOME/src/dotfiles"
 
 git_status=$(git status)
 
 if [[ "$git_status" == *"nothing to commit, working tree clean"* ]]; then
-    echo "No changes found in the repository. Exiting..."
+    echo "-- No changes found in the repository. Exiting..."
 else
     git add .
     git commit -m "dotfiles update ${timestamp}"
     git push origin master
-    echo "Update complete."
+    echo "-- Update complete."
 fi
 
 echo "========================================="
