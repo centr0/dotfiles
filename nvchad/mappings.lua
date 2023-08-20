48931bd192bfe5ab4529c9396c8e3a08389e97ae
Chris Paraiso <cparaiso@unicon.net>
2023-08-20 00:16:52 -0700

    dotfiles update 20230820001652

--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -1,6 +1,16 @@
 -- custom mappings
 
 local M = {}
+-- override nvchad bindings
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left"},
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right"},
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down"},
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up"},
+
+  }
+}
 
 -- disable default keymap
 M.disabled = {
@@ -11,12 +21,15 @@ M.disabled = {
 
 M.telescope = {
   n = {
-      ["<leader>fs"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+    ["<leader>fs"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
   }
 }
 
 -- my custom mappings
 M.custom = {
-
+  n = {
+    ["<leader>sv"] = {"<cmd>vs<CR>", "Split vertically"},
+    ["<leader>sh"] = {"<cmd>sp<CR>", "Split horizonally"}
+  }
 }
 return M
