f8a4a8e79a86b0768c23408f7c30c23e278d6fd8
Chris Paraiso <cparaiso@unicon.net>
2023-08-30 14:50:46 -0700

    dotfiles update 20230830145046

--- nvchad/configs/lspconfig.lua
+++ nvchad/configs/lspconfig.lua
@@ -2,6 +2,7 @@ local on_attach = require('plugins.configs.lspconfig').on_attach
 local capabilities = require('plugins.configs.lspconfig').capabilities
 
 local lspconfig = require 'lspconfig'
+local util = require "lspconfig/util"
 
 lspconfig.pyright.setup({
   on_attach = on_attach,
@@ -18,3 +19,17 @@ lspconfig.pyright.setup({
 })
 
 lspconfig.tsserver.setup({})
+
+lspconfig.gopls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {"gopls", "serve"},
+    filetypes = {"go", "gomod"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        analyses = {
+            unusedparams = true,
+        },
+        staticcheck = true,
+    },
+})
