97b32b35fdc05228b2494c1d43a053e987e7aa5f
Chris Paraiso <cparaiso@unicon.net>
2023-09-01 12:05:34 -0700

    dotfiles update 20230901120534

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -57,6 +57,16 @@ local plugins = {
         ft = {'go', 'gomod'},
         build = ':lua require("go.install").update_all_sync()'
     },
+    {
+        'gen740/SmoothCursor.nvim',
+        lazy = false,
+        config = function()
+            require('smoothcursor').setup({
+                autostart = true,
+                speed = 25,
+            })
+        end
+    }
 }
 
 return plugins
