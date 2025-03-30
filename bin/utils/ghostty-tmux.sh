f50697b9a1e874ecd2b27da82c75cee94cf0d16d
centro <centroscape@gmail.com>
2025-03-30 02:45:57 -0700

    dotfiles update

---
+++ bin/utils/ghostty-tmux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SESSION_NAME="centro"
+
+# Check if the session already exists
+/usr/local/bin/tmux has-session -t $SESSION_NAME 2>/dev/null
+
+if [ $? -eq 0 ]; then
+  # If the session exists, reattach to it
+  /usr/local/bin/tmux attach-session -t $SESSION_NAME
+else
+  # If the session doesn't exist, start a new one
+  /usr/local/bin/tmux new-session -s $SESSION_NAME -d
+  /usr/local/bin/tmux attach-session -t $SESSION_NAME
+fi
