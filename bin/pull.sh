129cf5477f4544ec3082b7d5988e3b26dedd9b35
Chris Paraiso <centroscape@gmail.com>
2025-05-03 16:24:14 -0700

    cleanup

--- bin/pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "========================================="
-cp "$HOME/.tmux.conf" "$HOME/src/dotfiles/tmux/tmux.conf"
-echo "-- Pulled your tmux configuration..."
-echo "-- Checking for changes..."
