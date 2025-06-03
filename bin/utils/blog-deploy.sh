6a19209327bee4a2467bc9d1c758c001feb471ea
centro <centroscape@gmail.com>
2025-06-03 00:00:25 -0700

    update

--- bin/utils/blog-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-PUBLIC_DIR="$HOME/src/projects/blog/cparaiso.github.io"
-cd "$PUBLIC_DIR"
-git add .
-git commit -m "sync $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin main
